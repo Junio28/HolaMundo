2345f4f18bbd9ded9d66745b31726085acf7367c
Junior28 <ejniormartinez@uniguajira.edu.co>
2020-10-15 22:23:55 -0500

    Poner al boton funcionando

--- app/src/main/java/com/example/holamundo/LoginActivity.java
+++ app/src/main/java/com/example/holamundo/LoginActivity.java
@@ -1,14 +1,28 @@
 package com.example.holamundo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button = findViewById(R.id.btngomain);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
